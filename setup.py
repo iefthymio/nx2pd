8b1f79c1c265328522a13e16729529482a584ceb
Efthymiopoulos Ilias <iefthymio@gmail.com>
2021-02-15 23:41:20 +0100

    update to version 0.02

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='nx2pd',
-      version='0.01',
+      version='0.02',
       description='A simple package for NXCALS',
       url='https://github.com/sterbini/nx2pd',
-      author='Guido STERBINI',
+      author='Guido STERBINI, Ilias Efthymiopoulos',
       author_email='guido.sterbini@cern.ch',
       license='GNU',
       packages=['nx2pd'],
